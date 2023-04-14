e80412aeadef21e41deecc217cde7ff4430568fc
Kalyan Kuna <83951390+kalyankuna124@users.noreply.github.com>
2023-04-14 09:56:23 +0530

    Add files via upload

---
+++ flames.py
@@ -0,0 +1,30 @@
+# input 2 names
+#findout common characers & remove that from name + reamining characters
+#based on the count find relaion
+name1=input("enter the name1=")
+name2=input("enter he name2=")
+
+for i in name1:
+    for j in name2:
+        if i==j:
+            name1=name1.replace(i,"",1)
+            name2=name2.replace(j,"",1)
+            break
+print(name1,name2)
+count=len(name1+name2)
+print(count)
+
+if count>0:
+    list1=["friends","lovers","affection","marriage","enemies","siblings"]
+    while len(list1)>1:
+        c=count%len(list1)
+        y=c-1
+        if y>=0:
+            left=list1[:y]
+            right=list1[y+1:]
+            list1=right+left
+        else:
+            list1=list1[:len(list1)-1]
+    print("relationship is:",list1[0])
+else:
+    print("differnt relationship")
